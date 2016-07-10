a9689f9e889fddd159e32dea560d16c3d291eb81
nimish <nimish.gupta@vinsol.com>
2016-07-10 16:24:46 +0530

    fix reimbursement issue

--- app/models/spree/reimbursment_decorator.rb
+++ app/models/spree/reimbursment_decorator.rb
@@ -1,16 +1,8 @@
 Spree::Reimbursement.class_eval do
   include Taxable
 
-  state_machine :reimbursement_status, initial: :pending do
-    after_transition to: :reimbursed, do: :remove_tax_for_returned_items
-    event :errored do
-      transition to: :errored, from: :pending
-    end
-
-    event :reimbursed do
-      transition to: :reimbursed, from: [:pending, :errored]
-    end
-  end
+  state_machine = self.state_machines[:reimbursement_status]
+  state_machine.after_transition to: [:reimbursed], do: :remove_tax_for_returned_items
 
   def remove_tax_for_returned_items
     return unless taxjar_applicable?(order)
