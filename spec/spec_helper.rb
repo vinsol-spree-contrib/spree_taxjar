a92312ada4804c7c47968ee0fd4952243947eafb
Sawan Gupta <sawan.gupta92@gmail.com>
2016-11-23 13:37:17 +0530

    Bump version to 3.2.0.beta1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 
 require 'shoulda-matchers'
+include Shoulda::Matchers::Independent
 require 'rspec/rails'
 require 'database_cleaner'
 require 'ffaker'
