60be8b65428c5f8c0f19597e160b06c6f7757095
Pikender Sharma <pikender@vinsol.com>
2017-01-13 19:01:32 +0530

    Added provision to enable/disable taxjar calculation

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,3 +1,4 @@
 Spree::AppConfiguration.class_eval do
   preference :taxjar_api_key, :string
+  preference :taxjar_enabled, :boolean, default: false
 end
--- app/models/spree/reimbursment_decorator.rb
+++ app/models/spree/reimbursment_decorator.rb
@@ -5,6 +5,7 @@ Spree::Reimbursement.class_eval do
   state_machine.after_transition to: [:reimbursed], do: :remove_tax_for_returned_items
 
   def remove_tax_for_returned_items
+    return unless Spree::Config[:taxjar_enabled]
     return unless taxjar_applicable?(order)
     client = Spree::Taxjar.new(order, self)
     client.create_refund_transaction_for_order
