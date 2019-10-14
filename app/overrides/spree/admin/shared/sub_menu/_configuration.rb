c6123fd91a46797fde46fa0a58941ca19a5b8703
Biruk H. Tabor <biruk.haileye@gmail.com>
2019-10-14 13:54:22 +0300

    check spree configuration management permission to show sidebar menu item

--- app/overrides/spree/admin/shared/sub_menu/_configuration.rb
+++ app/overrides/spree/admin/shared/sub_menu/_configuration.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path:  "spree/admin/shared/sub_menu/_configuration",
   name: "add_taxjar_admin_menu_link",
   insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
-  text: "<%= configurations_sidebar_menu_item 'Taxjar Settings', edit_admin_taxjar_settings_path %>"
+  text: "<%= configurations_sidebar_menu_item 'Taxjar Settings', edit_admin_taxjar_settings_path if can? :manage, Spree::Config %>"
 )
